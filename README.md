1914e0e1dac3fc6baf689178d2c4e2d4cba77159
xTruog94 <quang9499@gmail.com>
2021-08-30 17:11:41 +0700

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1> LogTelegram </h1>
+LogTelegram is a Python package that contains quick send message to your Telegram Chat Box.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='LogTelegram',
+    version='0.0.1',
+    author='xTruoq',
+    author_email='truongnx@websosanh.com',
+    description='Quickly Logs to Telegram',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/xTruog94/LogTelegram',
+    project_urls = {
+        "Bug Tracker": "https://github.com/xTruog94/LogTelegram/issues"
+    },
+    license='MIT',
+    packages=['LogTelegram'],
+    install_requires=['requests'],
+)
